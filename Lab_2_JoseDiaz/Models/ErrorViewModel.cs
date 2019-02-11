fcdae2d30957d837ed2139d05fbfaa23ba12f859
JOSE JORGE DIAZ MONTERROSO <jjdiazmo@correo.url.edu.gt>
2019-02-11 16:16:25 -0600

    Creacion del proyecto

---
+++ Lab_2_JoseDiaz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_2_JoseDiaz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
