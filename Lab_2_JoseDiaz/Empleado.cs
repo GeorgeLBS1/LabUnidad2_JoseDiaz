09cdc378de5ecf316783c304d7fa9161241f88b6
JOSE JORGE DIAZ MONTERROSO <jjdiazmo@correo.url.edu.gt>
2019-02-11 16:36:44 -0600

    Se creo la clase emmpleado con sus respectivas variables

---
+++ Lab_2_JoseDiaz/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Timers;
+
+namespace Lab_2_JoseDiaz
+{
+    public class Empleado
+    {
+        public string nombre;
+        public int codigo_empleado;
+        public double horas_trabajadas;
+        public bool Esta_En_La_Oficina;
+        public double Sueldo;
+        const double Pago_Hora = 38.00;
+    }
+}
